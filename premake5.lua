c6c31ea0388036ff68163953087ffe935a2aad51
Mason Luna <mason.luna24@yahoo.com>
2024-12-10 11:11:30 -0600

    Implemented build system setup

---
+++ premake5.lua
@@ -0,0 +1,19 @@
+workspace "Scribble2D"
+    architecture "x64"
+    startproject "Scribble2D-Core"
+
+    configurations {
+        "Debug",
+        "Release"
+    }
+
+    outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+    IncludeDir = {}
+    IncludeDir["GLFW"] = "Scribble2D-Core/vendor/GLFW/include"
+    IncludeDir["Glad"] = "Scribble2D-Core/vendor/Glad/include"
+    IncludeDir["glm"]  = "Scribble2D-Core/vendor/glm/glm-1.0.1"
+    IncludeDir["spdlog"] = "Scribble2D-Core/vendor/spdlog/include"
+
+    include "Scribble2D-Core"
+    include "Scribble2D-Sandbox"
