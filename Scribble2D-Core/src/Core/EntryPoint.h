a8dc9205d0beff3a1d6c9f1c2c62e093dbc219ec
Mason Luna <mason.luna24@yahoo.com>
2024-12-18 20:49:32 -0600

    header guard was useless

--- Scribble2D-Core/src/Core/EntryPoint.h
+++ Scribble2D-Core/src/Core/EntryPoint.h
@@ -3,9 +3,6 @@
 
 extern Scribble::Application* Scribble::CreateApplication();
 
-#ifndef SCB_MAIN_DEFINED
-#define SCB_MAIN_DEFINED
-
 int main(int argc, char** argv)
 {
 	Scribble::Log::Init();
@@ -15,5 +12,3 @@ int main(int argc, char** argv)
 	app->Run();
 	delete app;
 }
-
-#endif
