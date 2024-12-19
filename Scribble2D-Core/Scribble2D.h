3a464cb3627da24b084ad6bb28fda154687b782a
Mason Luna <mason.luna24@yahoo.com>
2024-12-18 20:25:14 -0600

    Updated Scribble2D.h

--- Scribble2D-Core/Scribble2D.h
+++ Scribble2D-Core/Scribble2D.h
@@ -2,10 +2,16 @@
 
 // Used by Scribble2D Applications
 
+// Core modules
 #include "src/Core/Application.h"
 #include "src/Core/Layer.h"
 #include "src/Core/Log.h"
+
+// Renderer modules
 #include "src/Renderer/Renderer.h"
+
+// Scene modules
+#include "Scene/Object.h"
 
 // ===Entry Point=======================
 #include "src/Core/EntryPoint.h"
