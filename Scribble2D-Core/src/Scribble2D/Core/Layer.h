945614c786f3d7d9beb4b082b0bba156408395fc
Mason Luna <mason.luna24@yahoo.com>
2024-12-19 00:08:33 -0600

    NOW I've implemented Timestep OnUpdate function

--- Scribble2D-Core/src/Scribble2D/Core/Layer.h
+++ Scribble2D-Core/src/Scribble2D/Core/Layer.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "Scribble2D/Events/Event.h"
-#include "Scribble2D/Core/Layer.h"
+#include "Scribble2D/Core/Timestep.h"
 
 
 namespace Scribble {
@@ -15,7 +15,7 @@ namespace Scribble {
 		virtual void OnAttach() {}
 		virtual void OnDetach() {}
 		virtual void OnUpdate() {}
-		virtual void OnUPdate() {}
+		virtual void OnUpdate(Timestep ts) {}
 		virtual void OnEvent(Event& e) {}
 
 	protected:
