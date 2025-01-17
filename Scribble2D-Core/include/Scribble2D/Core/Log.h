cbe954d0e568298e0b16d17ce00979bddc4a890f
Mason Luna <mason.luna24@yahoo.com>
2025-01-17 14:37:20 -0600

    Engine shouldn't be able to say whether app can use logger

--- Scribble2D-Core/include/Scribble2D/Core/Log.h
+++ Scribble2D-Core/include/Scribble2D/Core/Log.h
@@ -28,21 +28,16 @@ namespace Scribble {
 #define SCB_CORE_WARN(...)  ::Scribble::Log::GetCoreLogger()->warn(__VA_ARGS__)
 #define SCB_CORE_FATAL(...) ::Scribble::Log::GetCoreLogger()->critical(__VA_ARGS__)
 
-#define SCB_ERROR(...) ::Scribble::Log::GetClientLogger()->error(__VA_ARGS__)
-#define SCB_TRACE(...) ::Scribble::Log::GetClientLogger()->trace(__VA_ARGS__)
-#define SCB_INFO(...)  ::Scribble::Log::GetClientLogger()->info(__VA_ARGS__)
-#define SCB_WARN(...)  ::Scribble::Log::GetClientLogger()->warn(__VA_ARGS__)
-#define SCB_FATAL(...) ::Scribble::Log::GetClientLogger()->critical(__VA_ARGS__)
 #else
 #define SCB_CORE_ERROR(...)
 #define SCB_CORE_TRACE(...)
 #define SCB_CORE_INFO(...)
 #define SCB_CORE_WARN(...)
 #define SCB_CORE_FATAL(...)
-
-#define SCB_ERROR(...)
-#define SCB_TRACE(...)
-#define SCB_INFO(...) 
-#define SCB_WARN(...)
-#define SCB_FATAL(...)
 #endif
+
+#define SCB_ERROR(...) ::Scribble::Log::GetClientLogger()->error(__VA_ARGS__)
+#define SCB_TRACE(...) ::Scribble::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define SCB_INFO(...)  ::Scribble::Log::GetClientLogger()->info(__VA_ARGS__)
+#define SCB_WARN(...)  ::Scribble::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define SCB_FATAL(...) ::Scribble::Log::GetClientLogger()->critical(__VA_ARGS__)
